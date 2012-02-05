27945939810af9209d4fe7599aabcd20775b4d7c
vineetk32@gmail.com <vineetk32@gmail.com@d420176d-33ff-df78-5b48-e6af6674ae32>
2012-02-05 05:07:03 +0000

    Dummy Commit - just added the include for mythread.h

--- trunk/libmutex/src/mymutex.h
+++ trunk/libmutex/src/mymutex.h
@@ -5,6 +5,8 @@ vineet Vineet Krishnan
 */
 #ifdef __MYMUTEX_H
 
+#include "mythread.h"
+
 int pthread_mutexattr_init(pthread_mutexattr_t *attr);
 
 int pthread_mutexattr_destroy(pthread_mutexattr_t *attr);
