1099f8e702ac257897b32e62d3f44fc0f2f05081
vineetk32 <vineetk32@gmail.com>
2012-02-11 05:19:38 +0000

    Done! (with mutex)

--- libmutex/src/mymutex.c
+++ libmutex/src/mymutex.c
@@ -24,15 +24,14 @@ int mythread_mutex_destroy(mythread_mutex_t *mutex)
 
 int mythread_mutex_lock(mythread_mutex_t *mutex)
 {
-	mythread_enter_kernel();
-	if (mutex->lock == MUTEX_UNLOCKED)
-	{
-		mutex->lock = MUTEX_LOCKED;
-		mythread_leave_kernel();
-	}
-	else
-	{
+	int status = __test_test_and_set(mutex);
+	if(status == -1){
+		mythread_enter_kernel();
 		mythread_block(&mutex->block_queue,0);
+	}
+	else if(status == 0)
+	{
+		return 0;
 	}
 }
 
@@ -40,14 +39,21 @@ int mythread_mutex_lock(mythread_mutex_t *mutex)
 int mythread_mutex_unlock(mythread_mutex_t *mutex)
 {
 	mythread_enter_kernel();
-	if (mutex->lock == MUTEX_LOCKED)
-	{
-		mythread_unblock(&mutex->block_queue,0);
-		mutex->lock = MUTEX_UNLOCKED;
-	}
-	else
-	{
-		mythread_leave_kernel();
-	}
+	mutex->lock = MUTEX_UNLOCKED;
+	mythread_leave_kernel();
 }
 
+int __test_test_and_set(mythread_mutex_t *mutex)
+{
+	int counter = 100;
+	while (counter > 0)
+	{
+		while ( mutex->lock == MUTEX_LOCKED);
+		if ( compare_and_swap(&mutex->lock,MUTEX_UNLOCKED,MUTEX_LOCKED) == MUTEX_UNLOCKED)
+		{
+			return 0;
+		}
+		counter--;
+	}
+	return -1;
+}
