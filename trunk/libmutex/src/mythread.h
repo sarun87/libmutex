dec1c14c961ce306a05d9c17d38dc314701a81a3
vineetk32@gmail.com <vineetk32@gmail.com@d420176d-33ff-df78-5b48-e6af6674ae32>
2012-02-12 03:21:29 +0000

    Added mycond and mymutex to mythread.h

--- trunk/libmutex/src/mythread.h
+++ trunk/libmutex/src/mythread.h
@@ -16,6 +16,8 @@
 //#include <pthread.h>
 #include "myqueue.h"
 #include "futex.h"
+#include "mymutex.h"
+#include "mycond.h"
 
 typedef struct mythread_attr { /* thread attributes */
   int attr;                    /* not yet used */
